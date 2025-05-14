28ab61d4e72f3b26a44188563e05122fd7463786
Jecília Teixeira <111911408+Jecilia@users.noreply.github.com>
2025-05-14 12:14:25 +0100

    Area and Pie charts with Recharts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-export default function Home() {
+import AppAreaChart from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarchart";
+import AppPieChart from "@/components/AppPieChart";
+
+export default function Homepage() {
   return (
-   <div className="">
+   <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <AppBarChart/>
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg">TESTE</div>
+    <div className="bg-primary-foreground p-4 rounded-lg">
+      <AppPieChart/>
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg">TESTE</div>
+    <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+      <AppAreaChart/>
+    </div>
+    <div className="bg-primary-foreground p-4 rounded-lg">TESTE</div>
     
    </div>
   )
